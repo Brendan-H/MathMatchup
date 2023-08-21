cd5addaf909d427baf3dd4038702d3bd5650ccd8
Brendan H <bjharan7@gmail.com>
2023-08-21 18:20:19 -0500

    beginning of flutter tests

--- math_matchup/test/widget_test.dart
+++ math_matchup/test/widget_test.dart
@@ -5,27 +5,72 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:math_matchup/src/app.dart';
+import 'package:math_matchup/src/features/homescreen/repository/create_game.dart';
+import 'package:math_matchup/src/features/homescreen/repository/join_game.dart';
 
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('createGame Test', (WidgetTester tester) async {
+    // Mock Dio responses
+    final dio = Dio();
+    dio.options = BaseOptions(validateStatus: (_) => true);
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          return handler.resolve(Response(data: {'gameCode': '123456'}, requestOptions: RequestOptions()));
+        },
+      ),
+    );
 
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Builder(
+          builder: (context) => ElevatedButton(
+            onPressed: () => createGame(context),
+            child: const Text('Create Game'),
+          ),
+        ),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Tap the button and wait for the asynchronous operations to complete
+    await tester.tap(find.text('Create Game'));
+    await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify the expected behavior, e.g., navigation, dialogs, etc.
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('joinGame Test', (WidgetTester tester) async {
+    // Mock Dio responses
+    final dio = Dio();
+    dio.options = BaseOptions(validateStatus: (_) => true);
+    dio.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          return handler.resolve(Response(data: {}, requestOptions: RequestOptions())); // Simulate a successful response
+        },
+      ),
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Builder(
+          builder: (context) => ElevatedButton(
+            onPressed: () => joinGame('123456', 'John', context),
+            child: const Text('Join Game'),
+          ),
+        ),
+      ),
+    );
+
+    // Tap the button and wait for the asynchronous operations to complete
+    await tester.tap(find.text('Join Game'));
+    await tester.pumpAndSettle();
+
+    // Verify the expected behavior, e.g., navigation, dialogs, etc.
   });
 }
