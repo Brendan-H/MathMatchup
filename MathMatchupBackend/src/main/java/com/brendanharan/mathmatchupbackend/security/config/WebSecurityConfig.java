4415227eac10d3079c04ae328838fb24fe98904a
Brendan H <bjharan7@gmail.com>
2024-09-05 17:33:23 -0500

    basic security but need to fix since spring security 3 sucks

--- MathMatchupBackend/src/main/java/com/brendanharan/mathmatchupbackend/security/config/WebSecurityConfig.java
+++ MathMatchupBackend/src/main/java/com/brendanharan/mathmatchupbackend/security/config/WebSecurityConfig.java
@@ -11,15 +11,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfig {
-//
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/users/**")).permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http.cors().and()
@@ -31,31 +42,31 @@ import org.springframework.web.filter.CommonsRequestLoggingFilter;
 //                .oauth2ResourceServer().jwt();
 //        return http.build();
 //    }
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowCredentials(true);
-//        corsConfiguration.addAllowedOrigin("https://your-frontend-domain.com");
-//        corsConfiguration.addAllowedHeader("*");
-//        corsConfiguration.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", corsConfiguration);
-//        return source;
-//    }
-//
-//    @Configuration
-//    public class RequestLoggingFilterConfig {
-//
-//        @Bean
-//        public CommonsRequestLoggingFilter logFilter() {
-//            CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
-//            filter.setIncludeQueryString(true);
-//            filter.setIncludePayload(true);
-//            filter.setMaxPayloadLength(10000);
-//            filter.setIncludeHeaders(false);
-//            filter.setAfterMessagePrefix("REQUEST DATA : ");
-//            return filter;
-//        }
-//    }
-//}
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("https://192.168.1.0");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+    @Configuration
+    public class RequestLoggingFilterConfig {
+
+        @Bean
+        public CommonsRequestLoggingFilter logFilter() {
+            CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+            filter.setIncludeQueryString(true);
+            filter.setIncludePayload(true);
+            filter.setMaxPayloadLength(10000);
+            filter.setIncludeHeaders(false);
+            filter.setAfterMessagePrefix("REQUEST DATA : ");
+            return filter;
+        }
+    }
+}
