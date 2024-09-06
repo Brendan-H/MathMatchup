63daf1d3f172faa9a9073e214e62a3ba62ef799f
Brendan H <bjharan7@gmail.com>
2024-09-05 21:34:17 -0500

    finally updated everything to stupid security 3 standards

--- MathMatchupBackend/src/main/java/com/brendanharan/mathmatchupbackend/security/config/WebSecurityConfig.java
+++ MathMatchupBackend/src/main/java/com/brendanharan/mathmatchupbackend/security/config/WebSecurityConfig.java
@@ -7,8 +7,11 @@
 package com.brendanharan.mathmatchupbackend.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
@@ -22,10 +25,14 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.cors(AbstractHttpConfigurer::disable);
         http
-                .authorizeHttpRequests(requests -> requests
-                        .requestMatchers(new AntPathRequestMatcher("/users/**")).permitAll()
-                        .anyRequest().authenticated());
+                .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/users/**").authenticated()
+                .anyRequest().permitAll()).oauth2ResourceServer((oauth2) -> oauth2
+                        .jwt(Customizer.withDefaults())
+                );
 
         return http.build();
     }
