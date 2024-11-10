7528d30c3f1e219fc3e3caec08f1f18f9cac6be4
Brendan H <bjharan7@gmail.com>
2024-11-10 16:53:34 -0600

    Added style to education admin page

--- nextjs_website/src/pages/education/admin.tsx
+++ nextjs_website/src/pages/education/admin.tsx
@@ -44,22 +44,27 @@ const AdminHomePage = () => {
                 <div style={styles.section}>
                     <h2>Your License</h2>
                     <p>100 Teachers</p>
+                    <p>$299.99 monthly</p>
+                    <p>$2.99 per teacher</p>
+                    <p>Renews: 12/31/2024</p>
                 </div>
                 <div style={styles.section}>
-                    <h2>View Teachers</h2>
+                    <h2>View Teachers on Your License</h2>
                     <ul>
                         <li>Teacher one</li>
                         <li>Teacher two</li>
                     </ul>
-                    <div style={styles.section}>
-                        <h2>Need to add a teacher to your license?</h2>
-                        <p>Do so here</p>
-                        <button style={styles.button}>Add Teacher</button>
-                    </div>
-                    <div style={styles.section}>
-                        <h2>Just starting out?</h2>
-                        <p>Upload your teachers via CSV</p>
-                        <button style={styles.button}>Upload CSV</button>
+                    <div style={styles.minicontainer}>
+                        <div style={styles.noshadowsection}>
+                            <h2>Need to add a teacher to your license?</h2>
+                            <p>Do so here</p>
+                            <button style={styles.button}>Add Teacher</button>
+                        </div>
+                        <div style={styles.noshadowsection}>
+                            <h2>Just starting out?</h2>
+                            <p>Upload your teachers via CSV</p>
+                            <button style={styles.button}>Upload CSV</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -70,11 +75,32 @@ const AdminHomePage = () => {
 const styles = {
     container: {
         display: 'flex' as const,
-        flexDirection: 'column' as const,
+        flexDirection: 'row' as const,
         padding: '20px',
+        backgroundColor: colors.appBarColor,
+        justifyContent: 'space-around',
+    },
+    minicontainer: {
+        display: 'flex' as const,
+        flexDirection: 'row' as const,
+        padding: '20px',
+        backgroundColor: colors.primaryContainer,
+        justifyContent: 'space-around',
+    },
+    noshadowsection: {
+        marginBottom: '20px',
+        padding: '20px',
+        backgroundColor: colors.primaryContainer,
+        borderRadius: '10px',
+        width: '45%',
     },
     section: {
         marginBottom: '20px',
+        padding: '20px',
+        backgroundColor: colors.primaryContainer,
+        borderRadius: '10px',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        width: '45%',
     },
     button: {
         padding: '10px 20px',
